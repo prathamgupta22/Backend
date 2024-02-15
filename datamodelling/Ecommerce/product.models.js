a95a167423a49e36b9ee8c84dd1fae298e8d7f06
prathamgupta22 <pratham.guptapg01@gmail.com>
2024-02-15 16:38:57 +0530

    Modelling learner

---
+++ datamodelling/Ecommerce/product.models.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    description: {
+      required: true,
+      type: String,
+    },
+    name: {
+      required: true,
+      type: String,
+    },
+    productImage: {
+      type: String,
+    },
+    price: {
+      type: number,
+      default: 0,
+    },
+    stock: {
+      default: 0,
+      type: Number,
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+export const Product = new mongoose.model("Product", productSchema);
